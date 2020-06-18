a4f72756b1defa3dfa76210cb8b1d7c63fcf89d3
shivamshrivastava0896 <42324181+shivamshrivastava0896@users.noreply.github.com>
2020-06-18 11:45:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,46 +15,9 @@ def sms_reply():
 
     # Create reply
     resp = MessagingResponse()
-    resp.message(" State                        State Code
- Andaman and Nicobar Islands  AN
- Andhra Pradesh               AP
- Andhra Pradesh (New)         AD
- Arunachal Pradesh            AR
- Assam                        AS
- Bihar                        BH
- Chandigarh                   CH
- Chattisgarh                  CT
- Dadra and Nagar Haveli       DN
- Daman and Diu                DD
- Delhi                        DL
- Goa                          GA
- Gujarat                      GJ
- Haryana                      HR
- Himachal Pradesh             HP
- Jammu and Kashmir            JK
- Jharkhand                    JH
- Karnataka                    KA
- Kerala                       KL
- Lakshadweep Islands          LD
- Madhya Pradesh               MP
- Maharashtra                  MH
- Manipur                      MN
- Meghalaya                    ME
- Mizoram                      MI
- Nagaland                     NL
- Odisha                       OR
- Pondicherry                  PY
- Punjab                       PB
- Rajasthan                    RJ
- Sikkim                       SK
- Tamil Nadu                   TN
- Telangana                    TS
- Tripura                      TR
- Uttar Pradesh                UP
- Uttarakhand                  UT
- West Bengal                  WB")
+    resp.message("You sent me this message: {}".format(msg)")
 
-    return str(resp)
+    return 0
 
 
 
