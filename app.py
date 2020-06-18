f5f336e45cbcbafca731f64bc8d133c9682865ea
shivamshrivastava0896 <42324181+shivamshrivastava0896@users.noreply.github.com>
2020-06-18 19:58:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,16 @@ def sms_reply():
     msg = request.form.get('Body')
     if "Hey" in msg:
         resp = MessagingResponse()
-        resp.message("Hey my Name Twilo , I will help you in 1. covid case number")
+        resp.message("Hey my Name Twilo , I will help you in 1. covid case number Please type covid ! "
+                    + ' ' +"2. Joke of the day please type Joke")
+        print(type(msg))
+        k= str(resp)
+        
+    elif "Joke" in msg:
+        url='https://official-joke-api.appspot.com/random_joke'
+        response=request.get(url).json()
+        resp = MessagingResponse()
+        resp.message(response['setup'] + response['punchline'])
         print(type(msg))
         k= str(resp)
 
