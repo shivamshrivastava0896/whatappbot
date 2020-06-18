b78313517adeab129eb3cc4ef31c5fbcce2f3f72
shivamshrivastava0896 <42324181+shivamshrivastava0896@users.noreply.github.com>
2020-06-18 19:23:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,23 +7,19 @@ app=Flask(__name__)
 def wel():
     return 'Hello guys'
 
+
 @app.route("/sms", methods=['POST'])
 def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Fetch the message
     msg = request.form.get('Body')
-    if msg =='Hey':
-        resp = MessagingResponse()
-        resp.message("Hello there , I will help you in these cases now : 1.Covid case")
-        resp.message()
-        res =str(resp)
-    else:
-        resp = MessagingResponse()
-        resp.message("Hello there,I will help you: 1.Covid case")
-        resp.message()
-        res =str(resp)
-        res = str(resp)
-    return res
+
+    # Create reply
+    resp = MessagingResponse()
+    resp.message("You sent me this message: {}".format(msg))
+    print(msg.type)
+
+    return str(resp)
 
 
 if __name__=='__main__':
